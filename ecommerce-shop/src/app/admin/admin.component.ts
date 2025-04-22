396f070668ea1053b712e483d4ec86315cbf5647
Uji-hue <ujeza.shatri@gmail.com>
2025-04-22 20:24:45 +0200

    save products to localStorage

--- ecommerce-shop/src/app/admin/admin.component.ts
+++ ecommerce-shop/src/app/admin/admin.component.ts
@@ -1,4 +1,4 @@
-import { Component, effect } from '@angular/core';
+import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { RouterOutlet, RouterLink, RouterLinkActive } from '@angular/router';
--- ecommerce-shop/src/app/products/products.component.ts
+++ ecommerce-shop/src/app/products/products.component.ts
@@ -2,6 +2,15 @@ import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 
+const DEFAULT_PRODUCT = {
+  id: "",
+  name: "",
+  description: "",
+  price: "",
+  image: "",
+  color: ""
+}
+
 @Component({
   selector: 'app-products',
   imports: [CommonModule, FormsModule],
@@ -9,55 +18,54 @@ import { FormsModule } from '@angular/forms';
   styleUrl: './products.component.scss'
 })
 export class ProductsComponent {
+
+  ngOnInit(): void {
+    const savedProducts = localStorage.getItem("products");
+    if(savedProducts){
+      this.products = JSON.parse(savedProducts);
+    }
+  }
+
+
   isProductModalOpen = false;
   isEditProductModalOpen = false;
 
-  products: any[] = [
-    {
-      id: 1,
-      name: "product1",
-      description: "description1",
-      price: 100,
-      image: "https://as1.ftcdn.net/v2/jpg/02/75/74/58/1000_F_275745846_slBI2EsTudIShef6hMliS6Oa123tC9Zv.jpg",
-      color: "brown",
-    }
-  ]
-  product: any = {
-    id: "",
-    name: "",
-    description: "",
-    price: "",
-    image: "",
-    color: ""
-  }
+  products: any[] = []
+
+  newProduct: any = DEFAULT_PRODUCT;
+  selectedProduct: any = DEFAULT_PRODUCT;
+
   createProduct(){
     this.products.push({
-      ...this.product,
+      ...this.newProduct,
       id: this.products.length + 1
     });
-    this.product = {
-      id: "",
-      name: "",
-      description: ""
-    }
+    this.updateLocalStorage();
+    this.newProduct = DEFAULT_PRODUCT;
     this.closeProductModal()
   }
+
   showProductModal() {
     this.isProductModalOpen = true;
   }
+
   selectProduct(id: number){
-    this.product = Object.assign({}, this.products.find(product => product.id === id));
+    this.selectedProduct = Object.assign({}, this.products.find(product => product.id === id));
     this.showEditProductModal();
   }
+
   save(){
-    const index = this.products.findIndex(product => product.id === this.product.id);
-    this.products[index] = this.product;
+    const index = this.products.findIndex(product => product.id === this.selectedProduct.id);
+    this.products[index] = this.selectedProduct;
+    this.updateLocalStorage();
     this.closeEditProductModal();
   }
+
   del(id: number){
     const index = this.products.findIndex(product => product.id === id);
     if (index > -1) {
       this.products.splice(index, 1); 
+      this.updateLocalStorage();
     }
   }
 
@@ -71,5 +79,9 @@ export class ProductsComponent {
 
   closeEditProductModal() {
     this.isEditProductModalOpen = false;
+  }
+
+  updateLocalStorage(){
+    localStorage.setItem("products", JSON.stringify(this.products));
   }
 }
